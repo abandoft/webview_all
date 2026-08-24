e9367ed8b54d030a03984f121a0472dcdf6ded48
moluopro <moluopro@qq.com>
2026-08-25 01:00:12 +0800

    fix(windows): keep native warnings on one line

--- webview_all_windows/windows/src/util/logging.h
+++ webview_all_windows/windows/src/util/logging.h
@@ -2,6 +2,7 @@
 
 #include <windows.h>
 
+#include <cstdio>
 #include <string>
 #include <string_view>
 
@@ -9,9 +10,13 @@ namespace webview_all_windows::util {
 
 inline void LogWarning(std::string_view message) {
   std::string output = "[webview_all_windows] ";
-  output.append(message);
+  for (const char character : message) {
+    output.push_back(character == '\r' || character == '\n' ? ' ' : character);
+  }
   output.append("\n");
   OutputDebugStringA(output.c_str());
+  std::fwrite(output.data(), sizeof(char), output.size(), stderr);
+  std::fflush(stderr);
 }
 
 } // namespace webview_all_windows::util
